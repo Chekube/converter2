a091b301646bcc734506ca1b0a2d0946882e2b28
Chekube <mdankaro@gmail.com>
2022-09-11 16:19:08 +0100

    retain currency

--- index.php
+++ index.php
@@ -16,9 +16,9 @@
             <form method="POST" action="">
                 <div class="form-inline">
                     <select name="val" class="form-control">
-                        <option value="PHP">PHP</option>
-                        <option value="USD">USD</option>
-                        <option value="NAIRA">NAIRA</option>
+                    <option value="PHP" <?php echo isset($_POST['val']) && $_POST['val'] == "PHP" ? 'selected' : ''; ?>>PHP</option>
+                        <option value="USD" <?php echo isset($_POST['val']) && $_POST['val'] == "USD" ? 'selected' : ''; ?>>USD</option>
+                        <option value="NAIRA" <?php echo isset($_POST['val']) && $_POST['val'] == "NAIRA" ? 'selected' : ''; ?>>NAIRA</option>
                     </select>
                     <input class="form-control" type="number" value="<?php echo isset($_POST['digit']) ? $_POST['digit'] : ''  ?>" name="digit"/>
                 </div>    
@@ -27,11 +27,11 @@
                     <label>Select Currency: </label>
                     <select name="currency" required="required" class="form-control">
                         <option value="">Select an option</option>
-                        <option value="USD">USD</option>
-                        <option value="Euro">Euro</option>
-                        <option value="PHP">PHP</option>
-                        <option value="Japanese Yen">Japanese Yen</option>
-                        <option value="NAIRA">NAIRA</option>
+                        <option value="USD" <?php echo isset($_POST['currency']) && $_POST['currency'] == "USD" ? 'selected' : ''; ?>>USD</option>
+                        <option value="Euro" <?php echo isset($_POST['currency']) && $_POST['currency'] == "Euro" ? 'selected' : ''; ?>>Euro</option>
+                        <option value="PHP" <?php echo isset($_POST['currency']) && $_POST['currency'] == "PHP" ? 'selected' : ''; ?>>PHP</option>
+                        <option value="Japanese Yen" <?php echo isset($_POST['currency']) && $_POST['currency'] == "Japanese Yen" ? 'selected' : ''; ?>>Japanese Yen</option>
+                        <option value="NAIRA" <?php echo isset($_POST['currency']) && $_POST['currency'] == "NAIRA" ? 'selected' : ''; ?>>NAIRA</option>
                     </select>
                     <br /><br />
                     <center><button type="submit" name="convert" class="btn btn-primary form-control" style="width:30%;">Convert</button></center>
